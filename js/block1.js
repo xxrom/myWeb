a65f31649a39dbde7a6a26238865977c6cde1374
Chernyshov <xpom55@mail.ru>
2018-06-29 00:08:44 +0300

    delete all unnecessary

--- js/block1.js
+++ js/block1.js
@@ -1,15 +1,6 @@
 const mainImg = document.getElementsByClassName('b1__img')[0];
 
 window.onscroll = function block1() {
-  console.log('scroll', window.pageYOffset);
-
   const top = Math.round(window.pageYOffset * 0.6);
-  console.log(top);
-  console.log(mainImg.style.top);
-
   mainImg.style.top = `${top}px`;
 };
-
-const red = document.getElementsByClassName('red')[0];
-
-red.style.color = 'red';
