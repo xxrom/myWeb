4f0970aa04158d84c97abb33bf5f535d66ab682f
Chernyshov <xpom55@mail.ru>
2018-07-01 21:41:28 +0300

    add block 4 with info about me

--- js/block3.js
+++ js/block3.js
@@ -3,11 +3,12 @@ const items = document.getElementsByClassName('b3__item');
 const wrapper = document.getElementsByClassName('b3__wrapper-items')[0];
 
 let i = 0;
+const visibleItems = 4;
 
 setInterval(() => {
   i += 1;
-  if (i > items.length - 4) {
+  if (i > items.length - visibleItems) {
     i = 0;
   }
-  wrapper.style.transform = `translate(-${i * 25}%)`;
+  wrapper.style.transform = `translate(-${i * (100 / visibleItems)}%)`;
 }, 3000);
