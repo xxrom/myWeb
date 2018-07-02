efbae7df0f76326309affd8ce2e45737143243c2
Chernyshov <xpom55@mail.ru>
2018-07-02 23:11:23 +0300

    throttle in block1

--- js/block1.js
+++ js/block1.js
@@ -1,6 +1,23 @@
+const throttleTime = 30;
 const mainImg = document.getElementsByClassName('b1__img')[0];
+let timeId;
+// let top;
 
-window.onscroll = function block1() {
+function calcImg() {
   const top = Math.round(window.pageYOffset * 0.6);
   mainImg.style.top = `${top}px`;
+
+  timeId = undefined;
+}
+
+window.onscroll = function scrollImg() {
+  console.log('scroll', timeId);
+  // console.log(window.pageYOffset);
+  // console.log(window.innerHeight);
+  if (
+    typeof timeId === 'undefined' &&
+    window.pageYOffset < window.innerHeight
+  ) {
+    timeId = setTimeout(calcImg, throttleTime);
+  }
 };
