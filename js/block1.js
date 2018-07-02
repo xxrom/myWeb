94118c7c41ed1f8237df3adf93ba98ad458fc6fd
Chernyshov <xpom55@mail.ru>
2018-07-03 00:15:59 +0300

    delete block1.js console.logs

--- js/block1.js
+++ js/block1.js
@@ -1,7 +1,6 @@
 const throttleTime = 30;
 const mainImg = document.getElementsByClassName('b1__img')[0];
 let timeId;
-// let top;
 
 function calcImg() {
   const top = Math.round(window.pageYOffset * 0.6);
@@ -11,9 +10,6 @@ function calcImg() {
 }
 
 window.onscroll = function scrollImg() {
-  console.log('scroll', timeId);
-  // console.log(window.pageYOffset);
-  // console.log(window.innerHeight);
   if (
     typeof timeId === 'undefined' &&
     window.pageYOffset < window.innerHeight
