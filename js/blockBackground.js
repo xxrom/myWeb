d872df0d9feb12dce894f6488af5fb823d2860b2
Chernyshov <xpom55@mail.ru>
2018-07-04 20:23:10 +0300

    add background class and show them on the top

--- js/blockBackground.js
+++ js/blockBackground.js
@@ -1,96 +1,106 @@
-const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext('2d');
 console.log(window.width);
-canvas.width = 0.5 * window.innerWidth;
-canvas.height = 0.5 * window.innerHeight;
-canvas.style.border = '1px solid red';
+function Background() {
+  console.log('test');
 
-ctx.beginPath();
-ctx.arc(60, 60, 60, 0, 2 * Math.PI);
-ctx.stroke();
+  this.myCanvas = document.getElementById('myCanvas');
+  console.log(this.myCanvas);
 
-const updateTime = 30;
-const step = 1;
+  this.ctx = this.myCanvas.getContext('2d');
+  console.log(window.width);
 
-const numberItems = 30;
-const items = [];
+  this.myCanvas.width = window.innerWidth;
+  this.myCanvas.height = 200;
 
-function init() {
-  for (let i = 0; i < numberItems; i++) {
-    items.push(generateNewItem());
-  }
-}
+  // this.myCanvas.style.border = '1px solid red';
 
-init();
+  this.updateTime = 30;
+  this.step = 1;
 
-function generateNewItem() {
-  return Math.random() > 0.5
-    ? {
-      x: Math.random() * canvas.width,
-      y: 0
-    }
-    : {
-      x: 0,
-      y: Math.random() * canvas.height
-    };
-}
+  this.numberItems = 10;
+  this.items = [];
 
-function calcDist(one, two) {
-  return Math.sqrt(Math.abs((one.x - two.x) * (one.x - two.x)) +
-      Math.abs((one.y - two.y) * (one.y - two.y)));
-}
-
-function makeLineByTwoPoints(one, two, ctx) {
-  ctx.moveTo(one.x, one.y);
-  ctx.lineTo(two.x, two.y);
-  ctx.stroke();
-}
-
-function findNearItems(arr, ctx) {
-  arr.forEach((obj) => {
-    arr.forEach((item) => {
-      if (calcDist(obj, item) < 100) {
-        makeLineByTwoPoints(obj, item, ctx);
+  this.generateNewItem = function generateNewItem() {
+    return Math.random() > 0.5
+      ? {
+        x: Math.random() * this.myCanvas.width,
+        y: 0
       }
+      : {
+        x: 0,
+        y: Math.random() * this.myCanvas.height
+      };
+  };
+
+  this.calcDist = function calcDist(one, two) {
+    return Math.sqrt(Math.abs((one.x - two.x) * (one.x - two.x)) +
+        Math.abs((one.y - two.y) * (one.y - two.y)));
+  };
+
+  this.makeLineByTwoPoints = function makeLineByTwoPoints(one, two) {
+    this.ctx.moveTo(one.x, one.y);
+    this.ctx.lineTo(two.x, two.y);
+    this.ctx.stroke();
+  };
+
+  this.findNearItems = function findNearItems() {
+    this.items.forEach((obj) => {
+      this.items.forEach((item) => {
+        if (this.calcDist(obj, item) < 70) {
+          this.makeLineByTwoPoints(obj, item);
+        }
+      });
     });
-  });
+  };
+
+  this.moveItem = function moveItem(index) {
+    this.items[index].x += this.step;
+    this.items[index].y += this.step;
+
+    if (
+      this.items[index].x > this.myCanvas.width ||
+      this.items[index].y > this.myCanvas.height
+    ) {
+      this.items[index] = this.generateNewItem();
+    }
+  };
+
+  this.run = function run(numberItems) {
+    this.numberItems = numberItems;
+    for (let i = 0; i < numberItems; i++) {
+      this.items.push(this.generateNewItem());
+    }
+
+    setInterval(() => {
+      this.ctx.clearRect(0, 0, this.myCanvas.width, this.myCanvas.height);
+
+      this.ctx.lineWidth = 2;
+      this.ctx.strokeStyle = '#00f';
+      this.items.forEach((item, index) => {
+        this.ctx.beginPath();
+        this.ctx.arc(item.x, item.y, 3, 0, 2 * Math.PI);
+        this.ctx.stroke();
+
+        this.moveItem(index);
+      });
+
+      this.ctx.lineWidth = 0.1;
+      this.ctx.strokeStyle = '#000';
+
+      this.findNearItems();
+    }, this.updateTime);
+  };
+
+  // myCanvas.addEventListener('mousemove', (e) => {
+  //   const bounds = myCanvas.getBoundingClientRect();
+  //   const mx = e.clientX - bounds.left;
+  //   const my = e.clientY - bounds.top;
+  //   console.log(`${mx} ${my}`);
+
+  //   ctx.beginPath();
+  //   ctx.arc(mx, my, 10, 0, 2 * Math.PI);
+  //   ctx.stroke();
+  // });
 }
 
-function moveItem(items, index) {
-  items[index].x += step;
-  items[index].y += step;
-
-  if (items[index].x > canvas.width || items[index].y > canvas.height) {
-    items[index] = generateNewItem();
-  }
-}
-
-setInterval(() => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  ctx.lineWidth = 2;
-  ctx.strokeStyle = '#00f';
-  items.forEach((item, index) => {
-    ctx.beginPath();
-    ctx.arc(item.x, item.y, 3, 0, 2 * Math.PI);
-    ctx.stroke();
-
-    moveItem(items, index);
-  });
-
-  ctx.lineWidth = 0.1;
-  ctx.strokeStyle = '#f00';
-
-  findNearItems(items, ctx);
-}, updateTime);
-
-canvas.addEventListener('mousemove', (e) => {
-  const bounds = canvas.getBoundingClientRect();
-  const mx = e.clientX - bounds.left;
-  const my = e.clientY - bounds.top;
-  console.log(`${mx} ${my}`);
-
-  ctx.beginPath();
-  ctx.arc(mx, my, 10, 0, 2 * Math.PI);
-  ctx.stroke();
-});
+const background = new Background();
+background.run(30);
