125cc7826d134a28f12dc0fb5b26d89c9436805d
Chernyshov <xpom55@mail.ru>
2018-07-04 22:09:16 +0300

    fix footer height for mobile

--- js/blockBackground.js
+++ js/blockBackground.js
@@ -7,12 +7,21 @@ function Background() {
 
   const Footer = document.getElementsByClassName('footer-text')[0];
   console.log(Footer.offsetHeight);
+  console.log(getComputedStyle(Footer).height.replace('px', ''));
+  console.log(getComputedStyle(Footer).paddingTop.replace('px', ''));
+  console.log(getComputedStyle(Footer).paddingBottom.replace('px', ''));
+
+  const footerHeight =
+    +getComputedStyle(Footer).height.replace('px', '') +
+    +getComputedStyle(Footer).paddingTop.replace('px', '') +
+    +getComputedStyle(Footer).paddingBottom.replace('px', '');
+  console.log(footerHeight);
 
   this.ctx = this.myCanvas.getContext('2d');
 
   this.myCanvas.width = window.innerWidth;
 
-  this.myCanvas.height = Footer.offsetHeight;
+  this.myCanvas.height = footerHeight;
 
   // this.myCanvas.style.border = '1px solid red';
 
