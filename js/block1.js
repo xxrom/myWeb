899caaacb88a64385b952b027ec66ca730ea6ffb
Chernyshov <xpom55@mail.ru>
2018-07-05 19:12:39 +0300

    test pageYOffset || doc.scrollTop

--- js/block1.js
+++ js/block1.js
@@ -1,10 +1,14 @@
-const throttleTime = 30;
+const throttleTime = 10;
 const mainImg = document.getElementsByClassName('b1__img')[0];
 let timeId;
 
 function calcImg() {
-  const top = Math.round(window.pageYOffset * 0.6);
-  mainImg.style.top = `${top}px`;
+  const doc = document.documentElement;
+  console.log('mainImg', doc.scrollTop);
+  const top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+
+  // const top = Math.round(window.pageYOffset * 0.6);
+  mainImg.style.top = `${0.6 * top}px`;
 
   timeId = undefined;
 }
