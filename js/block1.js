03c86a1dcb99bf23f44e1c4a469cd435cef66796
Chernyshov <xpom55@mail.ru>
2018-07-05 19:40:06 +0300

    test block1 scroll by setInterval

--- js/block1.js
+++ js/block1.js
@@ -1,16 +1,35 @@
 const throttleTime = 10;
 const mainImg = document.getElementsByClassName('b1__img')[0];
 let timeId;
+const moveStep = 5;
 
 function calcImg() {
+  if (window.pageYOffset > window.innerHeight) {
+    clearInterval(timeId);
+    timeId = undefined;
+  }
+
   const doc = document.documentElement;
-  console.log('mainImg', doc.scrollTop);
-  const top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+  const top =
+    0.6 * ((window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0));
+  console.log(`top ${top}`);
+  let newTop = mainImg.style.top;
+  newTop = +newTop.replace('px', '');
 
-  // const top = Math.round(window.pageYOffset * 0.6);
-  mainImg.style.top = `${0.6 * top}px`;
+  console.log(`begin ${newTop}`);
+  console.log('sum ', newTop + moveStep);
 
-  timeId = undefined;
+  if (newTop + moveStep < top) {
+    newTop += moveStep;
+    mainImg.style.top = `${newTop}px`;
+  } else if (newTop - moveStep > top) {
+    newTop -= moveStep;
+    mainImg.style.top = `${newTop}px`;
+  } else {
+    console.log('EXIT');
+    clearInterval(timeId);
+    timeId = undefined;
+  }
 }
 
 window.onscroll = function scrollImg() {
@@ -18,6 +37,6 @@ window.onscroll = function scrollImg() {
     typeof timeId === 'undefined' &&
     window.pageYOffset < window.innerHeight
   ) {
-    timeId = setTimeout(calcImg, throttleTime);
+    timeId = setInterval(calcImg, throttleTime);
   }
 };
