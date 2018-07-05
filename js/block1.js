eedfc5a10b51ea39cc07d614942521d14a6e90b8
Chernyshov <xpom55@mail.ru>
2018-07-05 19:58:57 +0300

    for top IMG style transform

--- js/block1.js
+++ js/block1.js
@@ -1,42 +1,17 @@
-const throttleTime = 10;
 const mainImg = document.getElementsByClassName('b1__img')[0];
-let timeId;
-const moveStep = 5;
 
-function calcImg() {
+function scrollImg() {
   if (window.pageYOffset > window.innerHeight) {
-    clearInterval(timeId);
-    timeId = undefined;
+    return;
   }
 
   const doc = document.documentElement;
   const top =
     0.6 * ((window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0));
-  console.log(`top ${top}`);
-  let newTop = mainImg.style.top;
-  newTop = +newTop.replace('px', '');
 
-  console.log(`begin ${newTop}`);
-  console.log('sum ', newTop + moveStep);
-
-  if (newTop + moveStep < top) {
-    newTop += moveStep;
-    mainImg.style.top = `${newTop}px`;
-  } else if (newTop - moveStep > top) {
-    newTop -= moveStep;
-    mainImg.style.top = `${newTop}px`;
-  } else {
-    console.log('EXIT');
-    clearInterval(timeId);
-    timeId = undefined;
-  }
+  mainImg.style.transform = `translate(0, ${top}px)`;
 }
 
-window.onscroll = function scrollImg() {
-  if (
-    typeof timeId === 'undefined' &&
-    window.pageYOffset < window.innerHeight
-  ) {
-    timeId = setInterval(calcImg, throttleTime);
-  }
-};
+scrollImg();
+
+window.onscroll = scrollImg;
