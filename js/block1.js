c240e73b76d9926fbc6608a65e57910ab534e043
Chernyshov <xpom55@mail.ru>
2018-07-05 20:04:11 +0300

    position: fixed for background img in block1

--- js/block1.js
+++ js/block1.js
@@ -1,17 +1,17 @@
-const mainImg = document.getElementsByClassName('b1__img')[0];
+// const mainImg = document.getElementsByClassName('b1__img')[0];
 
-function scrollImg() {
-  if (window.pageYOffset > window.innerHeight) {
-    return;
-  }
+// function scrollImg() {
+//   if (window.pageYOffset > window.innerHeight) {
+//     return;
+//   }
 
-  const doc = document.documentElement;
-  const top =
-    0.6 * ((window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0));
+//   const doc = document.documentElement;
+//   const top =
+//     0.6 * ((window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0));
 
-  mainImg.style.transform = `translate(0, ${top}px)`;
+//   mainImg.style.transform = `translate(0, ${top}px)`;
 }
 
-scrollImg();
+// scrollImg();
 
-window.onscroll = scrollImg;
+// window.onscroll = scrollImg;
