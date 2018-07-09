b7eb2404d509296ce9476bc84dde71493fe5bd76
Chernyshov <xpom55@mail.ru>
2018-07-09 17:39:59 +0300

    add paralax for background img in block1, change css for block1, height will be changed only if diff between currentHeight and window.innerHeight would be more then 40% from currentHeight, bug fix for firefox with dynamic urlbar

--- js/block1.js
+++ js/block1.js
@@ -1,19 +1,42 @@
 const mainImg = document.getElementsByClassName('b1__img')[0];
+const block = document.getElementsByClassName('block1')[0];
+
+block.style.height = `${window.innerHeight}px`;
+
+let currentHeight = getComputedStyle(block).height.replace('px', '');
 
 function scrollImg() {
-  if (window.pageYOffset > window.innerHeight) {
+  if (window.pageYOffset > currentHeight) {
     mainImg.style.display = 'none';
-    return;
+    return; // выходим, блок не видно
   }
   mainImg.style.display = 'block';
 
-  // const doc = document.documentElement;
-  // const top =
-  //   0.6 * ((window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0));
+  parallax(mainImg, 0.5);
+}
 
-  // mainImg.style.transform = `translate(0, ${top}px)`;
+function resizeWindow() {
+  currentHeight = getComputedStyle(block).height.replace('px', '');
+  changeHeight(block, 0.4, currentHeight, window.innerHeight);
+}
+
+function parallax(item, coefficient) {
+  const doc = document.documentElement;
+  const top =
+    coefficient *
+    ((window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0));
+
+  item.style.transform = `translate(0, ${top}px)`;
+}
+
+function changeHeight(item, coefficient, current, maxHeight) {
+  if (Math.abs(current - maxHeight) > coefficient * current) {
+    block.style.height = `${maxHeight}px`;
+    console.log('resize');
+  }
 }
 
 scrollImg();
 
 window.onscroll = scrollImg;
+window.onresize = resizeWindow;
